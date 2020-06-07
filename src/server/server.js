9417f7aa62901a99864462d4b3178d5ac3c51b48
hassan635 <hassan.a.tariq@gmail.com>
2020-06-07 22:13:31 +1000

    tested query method

--- src/server/server.js
+++ src/server/server.js
@@ -7,6 +7,10 @@ app.get("/", (req, res) => {
     res.send("Assalam-o-Alaikum");
 });
 
+app.get("/request/url", (req, res) => {
+    res.send("Request Url is: " + req.query.id);
+});
+
 var server = http.createServer(app).listen(8081);
 
 console.log("Listening ON prt: " + server.address().port);
