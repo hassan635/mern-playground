28aa4d12f093fc7bb00c2df179775fc657102339
hassan635 <hassan.a.tariq@gmail.com>
2020-06-07 22:18:28 +1000

    tested param method

--- src/server/server.js
+++ src/server/server.js
@@ -8,7 +8,11 @@ app.get("/", (req, res) => {
 });
 
 app.get("/request/url", (req, res) => {
-    res.send("Request Url is: " + req.query.id);
+    res.send("Request query id is: " + req.query.id);
+});
+
+app.get("/request/url/:region", (req, res) => {
+    res.send("Request query region is: " + req.params.region)
 });
 
 var server = http.createServer(app).listen(8081);
