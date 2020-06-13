95c84a7910c438df3ed31f0ce0e2a21cd3dbe524
hassan635 <hassan.a.tariq@gmail.com>
2020-06-13 21:06:48 +1000

    Added redirects

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -5,13 +5,21 @@ router.get("/about", (req, res) => {
     res.send(`Captured request path ${req.baseUrl} in routes`);
 });
 
+router.get("/admin-panel", (req,res) => {
+    res.redirect("/")
+});
+
+router.get("/login", (req, res) => {
+    res.send("Login Page");
+});
+
 router.post("/json-endpoint", (req, res) => {
     if(req.query.isvalid === "valid")
     {
         res.sendStatus(200).send(req.body);
     }
     else{
-        res.sendStatus(400).send(req.body);
+        res.sendStatus(400).send({"message": "Invalid request"});
     }
 });
 
