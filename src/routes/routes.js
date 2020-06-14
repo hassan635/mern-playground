188760e871578048ba96cba161c1affec75f1c28
hassan635 <hassan.a.tariq@gmail.com>
2020-06-14 21:45:16 +1000

    Fixed error messages on error codes

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+router.use(express.json());
 
 router.get("/about", (req, res) => {
     res.send(`Captured request path ${req.baseUrl} in routes`);
@@ -17,10 +18,10 @@ router.get("/login", (req, res) => {
 router.post("/json-endpoint", (req, res) => {
     if(req.query.isvalid === "valid")
     {
-        res.sendStatus(200).send(req.body);
+        res.json(req.body);
     }
     else{
-        res.sendStatus(400).sendJson({"message": "Invalid request"});
+        res.status(400).json({message: "Invalid request"});
     }
 });
 
